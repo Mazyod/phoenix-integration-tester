1c00d2e1778688c4e70c4da8f949b4c7e7f55f78
Maz Jaleel <mazjaleel@gmail.com>
2016-11-25 10:55:00 +0100

    added some test events

--- web/channels/tester_channel.ex
+++ web/channels/tester_channel.ex
@@ -1,12 +1,21 @@
 defmodule Tester.TesterChannel do
   use Tester.Web, :channel
-
+  require Logger
 
   def join("tester:" <> _id, _payload, socket) do
     send self(), :after_join
     {:ok, socket}
   end
 
+  def handle_in("push_test", params, socket) do
+    Logger.debug("[push_test]: #{inspect params}")
+    {:reply, {:ok, params}, socket}
+  end
+
+  def handle_in("error_test", _, socket) do
+    Logger.debug("[error_test]")
+    {:reply, :error, socket}
+  end
 
   def handle_info(:after_join, socket) do
     push socket, "after_join", %{message: "Welcome!"}
