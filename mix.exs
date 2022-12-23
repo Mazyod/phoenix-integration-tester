f487357777e3e800a8686a37dad230e09a70bed9
Mazyad Alabduljaleel <mazjaleel@gmail.com>
2022-12-23 20:11:31 +0300

    bump deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tester.Mixfile do
   def project do
     [app: :tester,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.14",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
